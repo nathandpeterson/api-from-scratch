19fe63b17400d0e86667342014da7bd34e6ad5b6
Nathan Daniel Peterson <natperson@gmail.com>
2017-10-20 06:14:21 -0700

    server running

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+app.disable('x-powered-by')
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.get('/', (req, res, next) => {
+  res.json({message: 'connected'}).status(200)
+})
+
+app.use((err, req, res, next) => {
+  const status = 500 || err.status
+  res.status(status).json({error: err})
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: { message: 'Not found' }})
+})
+
+const listener = () => console.log(`-------listening on ${port}-------`)
+app.listen(port, listener)
